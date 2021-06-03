b51778e8aa96a7834c2d4cfe359b2080f6cf1d47
Thomas Steenbergen <thomas.steenbergen@here.com>
2021-06-03 20:45:59 +0200

    reporter-webapp: Set license color based on ScanCode's classification
    Signed-off-by: Thomas Steenbergen <thomas.steenbergen@here.com>

--- reporter-web-app/src/utils/index.js
+++ reporter-web-app/src/utils/index.js
@@ -40,4 +40,54 @@ const randomStringGenerator = (length = (Math.floor(Math.random() * 501) + 20)) 
     return alpha(l, symbol(1, alpha(r, []))).join('');
 };
 
-export { randomStringGenerator };
+// Utility function to generate color for given license string 
+const licenseToHslColor = (license) => {
+    let hash = 0;
+    let min;
+    let max;
+    // Basic license classification based on https://scancode-licensedb.aboutcode.org
+    const copyleft = new RegExp(
+        'AGPL|BSD-Protection|CAL|CC-BY-SA-4.0|CECILL-1.0|CPAL-1.0|eclipse-sua|ecosrh|EUPL|ghostscript|gust|Glide|' +
+        'GPL|hacos|ldpc|ldpgpl|lppl|NPOSL|Motosoto|ODbL-1.0|RHeCos-1.1|sleepycat|snia|SugarCRM|tanuki|tgppl|TMate|' +
+        'TOSL|vhfpl-1.0|Vim|VOSTROM|YPL-1.1',
+        'g'
+    );
+    const copyleftLimited = new RegExp(
+        'acdl-1.0|APSL|aptana|Artistic|autoconf|avisynth|bison|BitTorrent|c-fsl-1.1|CATOSL|CC-BY-SA|CDDL|' +
+        'CDLA-Sharing-1.0|CECILL|CERN-OHL-W-2.0|CPL-1.0|CUA-OPL-1.0|divx-open|dpl-1.1|eCos|EPL|EUPL|Eurosym|' +
+        'FreeImage|GFDL|GL2PS|gnuplot|GPL-2.0-with-|GPL-3.0-with-|GPL-3.0-linking|gSOAP-1.3|IPL-1.0|Imlib2|' +
+        'IPA|LGPL|LPL|MPL|MS-RL|NASA|NBPL|NGPL|Nokia|Noweb|NPL|OCCT|OCLC|OCLC|OPL|OGTSL|OLDAP|OSET-PL|Qhull|' +
+        'QPL-1.0|RSCPL|RPL|RPSL|Ruby|Sendmail|SMPPL|SPL-1.0|TAPR-OHL-1.0|TORQUE-1.1|tpl-1.0|truecrypt-3.1|' +
+        'UCL-1.0|vpl-1|wxWindows|YPL-1.0|Zimbra',
+        'g'
+    );
+
+    if (license.match(copyleftLimited)) {
+        min = 22;
+        max = 48;
+    } else if (license.match(copyleft)) {
+        min = 325;
+        max = 360;
+    } else {
+        min = 65
+        max = 290;
+    }
+
+    const hue = Math.floor(Math.random() * (max - min) + min);
+
+    for (let i = 0; i < license.length; i++) {
+        hash = license.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    if (min >= 65 && max <= 264) {
+        return ((hash) => {
+            return 'hsl(' + hue + ',' +
+            (25 + 70 * Math.random()) + '%,' + 
+            (55 + 10 * Math.random()) + '%)'
+        })();
+    } else {
+        return 'hsl(' + hue + ',100%,50%)'
+    }
+}
+
+export { randomStringGenerator, licenseToHslColor };
